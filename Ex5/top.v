03bb333b101f5509483d852ad303bba7761e2084
Cloud User <centos@ip-172-31-45-13.eu-west-2.compute.internal>
2021-05-25 13:02:13 +0000

    updated ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Morgane Breabout
+// Date: 25 May 2021
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,36 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+
+module aircond (clk, temperature, heating, cooling);
+    //Todo: add ports 
+	input clk;
+	input [4:0] temperature;
+	output reg [1:0] heating;
+	output reg [1:0]cooling; 
+
+    //Todo: add registers and wires, if needed
+
+    //Todo: add user logic
+	always @(posedge clk) 
+		begin
+		if (temperature<=4'd18) // if cold, heating
+			begin
+			heating<=1'd1;
+			cooling<=1'd0;
+			end
+		else if (temperature>=4'd20) //idle
+			(heating<=1'd0);
+		if (temperature>=4'd22) // if hot, cooling
+			begin
+			cooling<=1'd1;
+			heating<=1'd0;
+			end
+		else if (temperature<=4'd20) //idle
+			(cooling<=1'd0);	
+		end     
+endmodule
+    
+	
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,56 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Morgane
+// Date: Breabout
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+
+//Todo: Regitsers and wires
+	reg clk;
+	reg [4:0] temperature;
+	reg err;
+	wire heating;
+	wire cooling;
+
+//Todo: Clock generation
+	initial
+	begin
+		clk = 1'b0;
+	forever
+		#(CLK_PERIOD/2) clk=~clk;
+	end
+
+//Todo: User logic
+	initial begin
+		temperature=4'd17; // testing starting with 17?
+		err=0;
+
+		forever begin
+			#CLK_PERIOD
+
+			if ((heating & (temperature>=4'd20))|(!heating & (temperature<=4'd18)) // test heating
+				begin
+				$display("***TEST FAILED! not the right state!***", temperature, heating, cooling);
+				err=1;
+				end
+
+			if ((cooling & (temperature<=4'd20))|(!cooling & (temperature>=4'd22)) // test cooling 
+				begin
+				$display("***TEST FAILED! not the right state!***", temperature, heating, cooling);
+				err=1;
+				end
+
+		end
+	end
+
+
 
