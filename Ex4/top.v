17f56282f6dbd6fedcd0ed050c318f18fa25047d
Cloud User <centos@ip-172-31-45-13.eu-west-2.compute.internal>
2021-05-25 10:52:53 +0000

    A complete solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Morgane Breabout
+// Date: 25 May 2021
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -16,3 +16,26 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+
+module lights (rst, clk, button, colour);
+    //Todo: add ports 
+	input rst, clk, button;
+	output reg [2:0] colour; 
+
+    //Todo: add registers and wires, if needed
+	
+    //Todo: add user logic
+	always @(posedge clk) 
+		begin
+		if (rst)
+			colour<=3'b000;
+		if (button & colour<=6)
+			colour<=colour+3'b001;
+		else if ((button & colour==6)|(button & colour==7))
+			colour<=3'b001;
+			
+		end     
+endmodule
+    
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,103 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Morgane Breabout
+// Date: 25 May 2020
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+
+//Todo: Regitsers and wires
+	reg clk;
+	reg rst;
+	reg button;
+	reg err;
+	wire [2:0] colour;
+	reg [2:0] a;
+
+//Todo: Clock generation
+	initial
+	begin
+		clk = 1'b0;
+	forever
+		#(CLK_PERIOD/2) clk=~clk;
+	end
+
+//Todo: User logic
+	initial begin
+		rst=1;
+		button=0;
+		err=0;
+
+		forever begin
+			#CLK_PERIOD
+			if ((colour!=3'b000)&rst) // testing for reset
+				begin
+				$display("***TEST FAILED! not the right colour!***", rst, button, colour);
+				err=1;
+				end
+
+			if (button & colour<=6) // test incrementing colour by 1 up to colour 6
+				begin 
+				a=colour;
+				#CLK_PERIOD
+				if (colour!=a+3'b001)
+					begin
+					$display("***TEST FAILED! not the right colour!***", rst, button, colour);
+					err=1;
+					end
+				end
+			else if (button & (colour==6 | colour == 7)) // test cases 6 and 7 incrementing
+				begin
+				#CLK_PERIOD
+				if (colour!=3'b001)
+					begin
+					$display("***TEST FAILED! not the right colour!***", rst, button, colour);
+					err=1;
+					end
+				end
+
+			if (!button) // test that doesn't move if not button
+				begin
+				a=colour;
+				#CLK_PERIOD
+				if (colour!=a)
+					begin
+					$display("***TEST FAILED! not the right colour!***", rst, button, colour);
+					err=1;
+					end
+				end
+
+			if (colour>=3'b111)
+				begin
+				$display("***TEST FAILED! not the right colour!***", rst, button, colour);
+				err=1;
+				end
+		end
+	end
+
+//Todo: Finish test, check for success
+	initial begin
+	#(500*CLK_PERIOD)
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+//Todo: Instantiate counter module
+	lights lights1 (
+	.button (button),
+	.rst (rst),
+	.colour (colour)
+	);
+ 
+endmodule 
+
 
