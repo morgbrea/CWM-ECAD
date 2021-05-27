b8d10c2906809e24a15c8cbbc3893ab07f1f8191
Cloud User <centos@ip-172-31-45-13.eu-west-2.compute.internal>
2021-05-27 12:53:43 +0000

    updated ex7

--- Ex7/multiplexer.v
+++ Ex7/multiplexer.v
@@ -22,13 +22,17 @@
 
 module doorbell(
 	//Todo: define inputs here
-	input a,
-	input b, 
+	input [23:0] a,
+	input [23:0] b, 
 	input sel,
-	output out
+	output [23:0] out
 	); 
 	//Todo: define registers and wires here
-	wire out;
+	wire a;
+	wire b;
+	wire sel;
+	reg out;
+
 
 	//Todo: define your logic here                 
 	assign #5 out = sel?b:a;
