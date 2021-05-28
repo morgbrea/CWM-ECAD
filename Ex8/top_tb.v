bb3837354e733a41bde51a7a637538573a463722
Cloud User <centos@ip-172-31-45-13.eu-west-2.compute.internal>
2021-05-28 09:00:53 +0000

    updated ex8

--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,40 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8  - Simple End-to-End Design
-// Student Name:
-// Date: 
+// Student Name: Morgane Breabout
+// Date: 28 May 2021
 //
 // Description: A testbench module to test Ex8
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+    
+//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+
+//Todo: Registers and wires
+	
+	
+//Todo: Clock generation
+	initial
+	begin
+		clk = 1'b0;
+	forever
+		#(CLK_PERIOD/2) clk=~clk;
+	end
+
+
+//Todo: Finish test, check for success
+	initial begin
+	#(500*CLK_PERIOD)
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+      
+//Todo: Instantiate counter module
+	
+ 
+endmodule 
